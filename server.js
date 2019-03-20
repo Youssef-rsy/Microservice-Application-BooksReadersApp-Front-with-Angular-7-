60080fdb4f39a5641f7da0e3a315b93e72c60d2f
Youssef-rsy <youssef.rsy@gmail.com>
2019-03-20 22:39:24 +0100

    correcting app name in the server.js file

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/BookReades'));
+app.use(express.static(__dirname + '/dist/book-reades'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/BookReades/index.html'));
+res.sendFile(path.join(__dirname+'/dist/book-reades/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
